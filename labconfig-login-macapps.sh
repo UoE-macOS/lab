312260af3eff0962f97807ebcd627bf72034bef5
Gavin Anderson <gav.anderson@ed.ac.uk>
2018-09-12 16:45:35 +0100

    Update labconfig-login-macapps.sh

--- labconfig-login-macapps.sh
+++ labconfig-login-macapps.sh
@@ -5,8 +5,8 @@
 # This script reads in the app link configs and creates or deletes the app synlinks as required
 #
 #
-# Date: Wed 11 Jul 2018 14:51:00 BST
-# Version: 0.1.2
+# Date: Thur 16 Aug 2018 14:51:00 BST
+# Version: 0.1.3
 # Author: dsavage
 #
 ######
@@ -49,11 +49,16 @@ do
   AppPath=`cat "${f}" | awk -F "+" '{print $1}'`
   if [ -L "$LinkPath" ]; then
     echo "Delete and recreate symlink: ${AppPath} ${LinkPath}"
-    rm -f "$LinkPath" 
-    ln -s "${AppPath}" "${LinkPath}"
+    rm -f "$LinkPath"
+    # only make links to valid points, typos happen...
+    if [ -d "${AppPath}" ]; then
+    	ln -s "${AppPath}" "${LinkPath}"
+    fi
   else
-    echo "Create symlink: ${AppPath} ${LinkPath}"
-    ln -s "${AppPath}" "${LinkPath}"
+    if [ -d "${AppPath}" ]; then
+    	echo "Create symlink: ${AppPath} ${LinkPath}"
+        ln -s "${AppPath}" "${LinkPath}"
+    fi
   fi
   # take action on each file. $f store current file name
   cat $f
@@ -83,5 +88,58 @@ cat "/tmp/CatList.txt" | ( while read Category;
            rm -d "${MacSD}/Applications/${Category}"
       fi
 done)
+
+# Fix office firstrun
+submit_diagnostic_data_to_microsoft=false
+
+
+DisableOffice2016FirstRun(){
+
+   # This function will disable the first run dialog windows for all Office 2016 apps.
+   # It will also set the desired diagnostic info settings for Office application.
+
+   /usr/bin/defaults write /Library/Preferences/com.microsoft."$app" kSubUIAppCompletedFirstRunSetup1507 -bool true
+   /usr/bin/defaults write /Library/Preferences/com.microsoft."$app" SendAllTelemetryEnabled -bool "$submit_diagnostic_data_to_microsoft"
+
+   # Outlook and OneNote require one additional first run setting to be disabled
+
+   if [[ $app == "Outlook" ]] || [[ $app == "onenote.mac" ]]; then
+
+     /usr/bin/defaults write /Library/Preferences/com.microsoft."$app" FirstRunExperienceCompletedO15 -bool true
+
+   fi
+
+}
+
+
+
+# Run the DisableOffice2016FirstRun function for each detected Office 2016
+# application to disable the first run dialogs for that Office 2016 application.
+
+if [[ -e "/Applications/Microsoft Excel.app" ]]; then
+	app=Excel
+	DisableOffice2016FirstRun
+fi
+
+if [[ -e "/Applications/Microsoft OneNote.app" ]]; then
+	app=onenote.mac
+	DisableOffice2016FirstRun
+fi
+
+if [[ -e "/Applications/Microsoft Outlook.app" ]]; then
+	app=Outlook
+	DisableOffice2016FirstRun
+fi
+
+if [[ -e "/Applications/Microsoft PowerPoint.app" ]]; then
+	app=Powerpoint
+	DisableOffice2016FirstRun
+fi
+
+if [[ -e "/Applications/Microsoft Word.app" ]]; then
+	app=Word
+	DisableOffice2016FirstRun
+fi
+
 
 exit 0;
